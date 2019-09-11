f2537613be81acf4cd1b3e783bd5992414963580
yichengjie <626659321@qq.com>
2019-09-11 14:24:51 +0800

    echo服务进一步加深

---
+++ echo-client2/src/main/java/com/yicj/study/EchoClient.java
@@ -0,0 +1,61 @@
+package com.yicj.study;
+
+import java.net.InetSocketAddress;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class EchoClient {
+	
+	private final String host ;
+	private final int port ;
+	
+	public EchoClient(String host, int port) {
+		this.host = host ;
+		this.port = port ;
+	}
+	
+	public void start() throws Exception {
+		
+		EventLoopGroup group = new NioEventLoopGroup() ;
+		try {
+			//创建bootstrap
+			Bootstrap b = new Bootstrap() ;
+			//指定EventLoopGroup以处理客户端时间，需要适用于NIO的实现
+			b.group(group)
+			//适用于NIO传输的Channel类型
+			.channel(NioSocketChannel.class)
+			//设置服务器的InetSocketAddress
+			.remoteAddress(new InetSocketAddress(host,port))
+			//创建Channel时，向ChannelPipeline中添加一个EchoClientHandler实例
+			.handler(new ChannelInitializer<SocketChannel>() {
+				@Override
+				protected void initChannel(SocketChannel ch) throws Exception {
+					ch.pipeline().addLast(new EchoClientHandler()) ;
+				}
+			}) ;
+			//下次试一试
+			//ChannelFuture bind = b.bind();
+			//Channel channel = bind.channel();
+			//channel.connect(new InetSocketAddress(host,port)) ;
+			//连接到远程节点，阻塞等待直到连接完成
+			ChannelFuture f = b.connect().sync() ;
+			//阻塞，直到Channel关闭
+			f.channel().closeFuture().sync() ;
+		} finally {
+			//关闭线程池并且四方所有的资源
+			group.shutdownGracefully();
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		String host = "127.0.0.1" ;
+		int port = 3003 ;
+		EchoClient client = new EchoClient(host, port) ;
+		client.start();
+	}
+}
---
+++ echo-server2/src/main/java/com/yicj/study/EchoServer.java
@@ -0,0 +1,53 @@
+package com.yicj.study;
+
+import java.net.InetSocketAddress;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class EchoServer {
+	private final int port ;
+	public EchoServer(int port) {
+		this.port = port ;
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		EchoServer server = new EchoServer(3003) ;
+		server.start(); 
+	}
+	
+	
+	public void start() throws InterruptedException {
+		final EchoServerHandler serverHandler = new EchoServerHandler() ;
+		//创建EventLoopGroup
+		EventLoopGroup group = new NioEventLoopGroup() ;
+		try {
+			//创建ServerBootstrap
+			ServerBootstrap b = new ServerBootstrap() ;
+			b.group(group)
+			.channel(NioServerSocketChannel.class)
+			//使用指定的端口设置套接字地址
+			.localAddress(new InetSocketAddress(port))
+			//添加一个EchoServerHandler到子Channel的ChannelPipeline
+			//EchoSeverHandler被标记为@Shareable，所以我们可以总是使用同样的实例
+			.childHandler(new ChannelInitializer<SocketChannel>() {
+				@Override
+				protected void initChannel(SocketChannel ch) throws Exception {
+					ch.pipeline().addLast(serverHandler) ;
+				}
+			}) ;
+			//异步的绑定服务器，调用sync阻塞等待，直到绑定完成
+			ChannelFuture f = b.bind().sync() ;
+			//获取Channel的CloseFuture，并且阻塞当前线程直到它完成
+			f.channel().closeFuture().sync() ;
+		} finally {
+			//关闭EventLoopGroup，释放所有的资源
+			group.shutdownGracefully().sync() ;
+		}
+	}
+
+}
